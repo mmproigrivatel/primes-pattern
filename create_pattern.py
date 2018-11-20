e21a66442af94aab0e0e6023215a854969914320
Vladimir Bushuev <vbushuev05@gmail.com>
2018-11-20 21:35:00 +0300

    Update create_pattern.py
    Ускорена проверка числа на простоту

--- create_pattern.py
+++ create_pattern.py
@@ -1,19 +1,18 @@
 from PIL import Image, ImageDraw
-from time import time
 
 def is_prime(a):
     r = False
-    for i in range(2, a):
+    for i in range(2, a//2):
         if a%i==0: r = True
     return r
 
-img = Image.new('RGB', (1920, 1080))
+img = Image.new('RGB', (100, 100))
 imgDraw = ImageDraw.Draw(img)
 pix = img.load()
 
 for x in range(img.size[0]):
     for y in range(img.size[1]):
         if not is_prime(x^y):
-            imgDraw.point((x, y), (0, 0, 128))
+            imgDraw.point((x, y), (0, 0, 255))
 
 img.save('xor.png')
